c528c6b6ea7aed5ba393f7edc4f81cb27565560e
Frankie <knobelf@student.ethz.ch>
2020-03-17 17:20:19 +0100

    - extended feature extraction - found out is useless, svd is better...

--- franz/feature_enhancer.py
+++ franz/feature_enhancer.py
@@ -1,7 +1,66 @@
 from scipy import sparse as sp
+import numpy as np
+
+## USER FEATURES (10000, 1)
+# Total movies seen by this user
+def usr_mov_cnt(csr_matrix: sp.csr_matrix):
+    return np.asmatrix(csr_matrix.getnnz(1)).transpose()
 
 
-def weight_bla_bla(csr_matrix: sp.csr_matrix):
-    print(csr_matrix.sum(0))
+# User rating sum
+def usr_mov_sum(csr_matrix: sp.csr_matrix):
+    return csr_matrix.sum(1)
 
-    return 0
+
+# User rating mean of all seen movies
+def usr_mov_mean_no_zero(csr_matrix: sp.csr_matrix):
+    return np.divide(usr_mov_sum(csr_matrix), usr_mov_cnt(csr_matrix))
+
+
+# User rating mean of all available movies
+def usr_mov_mean(csr_matrix: sp.csr_matrix):
+    return csr_matrix.mean(1)
+
+
+# User rating var of all seen movies
+def usr_mov_var_no_zero(csr_matrix: sp.csr_matrix):
+    return usr_mov_mean_no_zero(csr_matrix.power(2)) - np.square(usr_mov_mean_no_zero(csr_matrix))
+
+
+# User rating var of all available movies
+def usr_mov_var(csr_matrix: sp.csr_matrix):
+    return usr_mov_mean(csr_matrix.power(2)) - np.square(usr_mov_mean(csr_matrix))
+
+
+## MOVIE FEATURES (1, 1000)
+# Number of people who have seen this movie
+def mov_usr_cnt(csr_matrix: sp.csr_matrix):
+    return np.asmatrix(csr_matrix.getnnz(0))
+
+
+# Sum of all ratings of this movie
+def mov_usr_sum(csr_matrix: sp.csr_matrix):
+    return csr_matrix.sum(0)
+
+
+# Movie rating mean of all who have seen this movie
+def mov_usr_mean_no_zero(csr_matrix: sp.csr_matrix):
+    return np.divide(mov_usr_sum(csr_matrix), mov_usr_cnt(csr_matrix))
+
+
+# Movie rating mean of all people
+def mov_usr_mean(csr_matrix: sp.csr_matrix):
+    return csr_matrix.mean(0)
+
+
+# Movie rating variance of all who have seen this movie
+def mov_usr_var_no_zero(csr_matrix: sp.csr_matrix):
+    return mov_usr_mean_no_zero(csr_matrix.power(2)) - np.square(mov_usr_mean_no_zero(csr_matrix))
+
+
+# Movie rating variance of all people
+def mov_usr_var(csr_matrix: sp.csr_matrix):
+    return mov_usr_mean(csr_matrix.power(2)) - np.square(mov_usr_mean(csr_matrix))
+
+
+# ALL USELESS... mache SVD -.-
--- franz/main.py
+++ franz/main.py
@@ -1,33 +1,3 @@
 from franz.importer import import_data
 import numpy as np
 from scipy import sparse as sp
-
-
-csr_matrix = import_data()
-
-# (1, 1000) How many users have seen this movie?
-usr_cnt = np.asmatrix(csr_matrix.getnnz(0))
-
-# (10000, 1) How many movies has this user seen?
-mov_cnt = np.asmatrix(csr_matrix.getnnz(1)).transpose()
-
-# (1, 1000) Sum of all ratings of this movie
-usr_sum = csr_matrix.sum(0)
-
-# (10000, 1) Sum of all ratings of this user
-mov_sum = csr_matrix.sum(1)
-
-# print(np.shape(np.mean(csr_matrix, 0)))
-# print(np.shape(np.mean(csr_matrix, 1)))
-matrix = csr_matrix.todense()
-matrix[matrix == 0] = np.nan
-
-print(np.nanmean(matrix, 1))
-# print(np.shape(np.var(csr_matrix, 1)))
-
-# (1, 1000) Mean rating of all ratings of this movie
-
-# print(np.shape(sp.h
-# Jede Reihe dividiert durch geschaute filme
-#print((sp.csr_matrix((csr_matrix.transpose() / np.asmatrix(csr_matrix.getnnz(0))).transpose())))
-# print(np.shape(np.divide(csr_matrix.sum(1), np.asmatrix(csr_matrix.getnnz(1)).transpose())))  # Was für ein Bewertungsdruchschnitt hat der user
