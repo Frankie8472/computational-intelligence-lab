868fa2ebf76db155524b4397ddcd135a7e5c64f8
Frankie <knobelf@student.ethz.ch>
2020-03-17 22:26:12 +0100

    - experimental imputer

--- franz/main.py
+++ franz/main.py
@@ -1,12 +1,17 @@
 from franz.porter import import_data, export_data
-#from franz.feature_enhancer import sparse_svd
 import numpy as np
+from sklearn.experimental import enable_iterative_imputer
+enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+#from franz.feature_enhancer import sparse_svd
 #from scipy import sparse as sp
 #from scipy.sparse.linalg import svds
 from fancyimpute import KNN, NuclearNormMinimization, SoftImpute, BiScaler
 
 data = import_data().todense()
 data[data == 0] = np.nan
+data_pred = IterativeImputer(verbose=2).fit_transform(data)
+
 # data_pred = BiScaler().fit_transform(data)
 # data_pred = SoftImpute().fit_transform(data_pred)
 
@@ -14,6 +19,6 @@ data[data == 0] = np.nan
 
 # data_pred = NuclearNormMinimization().fit_transform(data)
 
-# export_data(data_pred, 'franz')
+export_data(data_pred, 'franz')
 
 
