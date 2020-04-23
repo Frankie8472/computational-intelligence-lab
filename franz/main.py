c1ee49d8c14544618b762f079297af9d34b8aa91
Franz Knobel <knobelf@student.ethz.ch>
2020-04-23 23:37:54 +0200

    implemented clf porter.py: update import functions for dataframe for use with embeddinglayer main.py: implemented keras clf, not rdy yet, just some fast coding without thinking much franzSubmission.csv: ignore pls

--- franz/main.py
+++ franz/main.py
@@ -1,24 +1,203 @@
-from franz.porter import import_data, export_data
 import numpy as np
-from sklearn.experimental import enable_iterative_imputer
-enable_iterative_imputer
-from sklearn.impute import IterativeImputer
-#from franz.feature_enhancer import sparse_svd
-#from scipy import sparse as sp
-#from scipy.sparse.linalg import svds
-from fancyimpute import KNN, NuclearNormMinimization, SoftImpute, BiScaler
+from keras import backend as K
+from keras.losses import mean_squared_error
+from keras.layers import Input, Embedding, Flatten, Dot, Dense, Dropout, BatchNormalization, Reshape, Add, Concatenate, \
+    Lambda
+from keras.models import Model
+from keras.optimizers import Adam
 
-data = import_data().todense()
-data[data == 0] = np.nan
-data_pred = IterativeImputer(verbose=2).fit_transform(data)
+from franz.porter import export_data, import_dataframe
+
+
+# from sklearn.experimental import enable_iterative_imputer
+# enable_iterative_imputer
+# from sklearn.impute import IterativeImputer
+# from franz.feature_enhancer import sparse_svd
+# from scipy import sparse as sp
+# from scipy.sparse.linalg import svds
+# from fancyimpute import KNN, NuclearNormMinimization, SoftImpute, BiScaler, MatrixFactorization
+
+# Import data
+
+
+def root_mean_squared_error(y_true, y_pred):
+    return K.sqrt(mean_squared_error(y_true, y_pred))
+
+
+def keras_dot_model(input_dim_1, input_dim_2, n_latent_factors=64):
+    user_input = Input(shape=(1,), name='user_input')
+    movie_input = Input(shape=(1,), name='movie_input')
+
+    user_embedding = Embedding(name='user_embedding', input_dim=input_dim_1, output_dim=n_latent_factors)(user_input)
+    movie_embedding = Embedding(name='movie_embedding', input_dim=input_dim_2, output_dim=n_latent_factors)(movie_input)
+
+    user_embedding_bias = Embedding(name='user_embedding_bias', input_dim=input_dim_1, output_dim=1)(user_input)
+    movie_embedding_bias = Embedding(name='movie_embedding_bias', input_dim=input_dim_2, output_dim=1)(movie_input)
+
+    user_vec = Flatten(name='FlattenUsers')(user_embedding)
+    movie_vec = Flatten(name='FlattenMovies')(movie_embedding)
+
+    user_embedding_bias = Flatten()(user_embedding_bias)
+    movie_embedding_bias = Flatten()(movie_embedding_bias)
+
+    user_vec = Dropout(0.5)(user_vec)
+    movie_vec = Dropout(0.5)(movie_vec)
+
+    merged = Dot(name='dot_product', normalize=True, axes=1)([user_vec, movie_vec])
+
+    biased = Add()([merged, user_embedding_bias, movie_embedding_bias])
+
+    nn = Dense(96, activation='relu')(biased)
+    nn = Dropout(0.5)(nn)
+
+    nn = BatchNormalization()(nn)
+
+    nn = Dense(1, activation='relu')(nn)
+
+    model = Model(inputs=[user_input, movie_input], outputs=nn)
+    #model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(lr=1e-3), loss=root_mean_squared_error)
+
+    return model
+
+
+def keras_nn_model(input_dim_1, input_dim_2, n_latent_factors=64):
+    user_input = Input(shape=(1,), name='user_input')
+    movie_input = Input(shape=(1,), name='movie_input')
+
+    user_embedding = Embedding(name='user_embedding', input_dim=input_dim_1, output_dim=n_latent_factors)(user_input)
+    movie_embedding = Embedding(name='movie_embedding', input_dim=input_dim_2, output_dim=n_latent_factors)(movie_input)
+
+    user_embedding_bias = Embedding(name='user_embedding_bias', input_dim=input_dim_1, output_dim=1)(user_input)
+    movie_embedding_bias = Embedding(name='movie_embedding_bias', input_dim=input_dim_2, output_dim=1)(movie_input)
+
+    user_vec = Flatten()(user_embedding)
+    movie_vec = Flatten()(movie_embedding)
+
+    user_embedding_bias = Flatten()(user_embedding_bias)
+    movie_embedding_bias = Flatten()(movie_embedding_bias)
+
+    user_vec = Dropout(0.5)(user_vec)
+    movie_vec = Dropout(0.5)(movie_vec)
+
+    merged = Concatenate(name='concatenate_of_latent_features')([user_vec, movie_vec])
+
+    biased = Add()([merged, user_embedding_bias, movie_embedding_bias])
+
+    nn = Dense(96, kernel_initializer='he_normal', activation='relu')(biased)
+    nn = Dropout(0.5)(nn)
+
+    nn = BatchNormalization()(nn)
+
+    nn = Dense(1, kernel_initializer='he_normal', activation='relu')(nn)
+
+    model = Model(inputs=[user_input, movie_input], outputs=nn)
+    #model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(lr=1e-3), loss=root_mean_squared_error)
+
+    return model
+
+
+def keras_nn_model_2(input_dim_1, input_dim_2, n_latent_factors=64):
+    user_input = Input(shape=(1,), name='user_input')
+    movie_input = Input(shape=(1,), name='movie_input')
+
+    user_embedding = Embedding(name='user_embedding', input_dim=input_dim_1, output_dim=n_latent_factors)(user_input)
+    movie_embedding = Embedding(name='movie_embedding', input_dim=input_dim_2, output_dim=n_latent_factors)(movie_input)
+
+    user_embedding_bias = Embedding(name='user_embedding_bias', input_dim=input_dim_1, output_dim=1)(user_input)
+    movie_embedding_bias = Embedding(name='movie_embedding_bias', input_dim=input_dim_2, output_dim=1)(movie_input)
+
+    user_vec = Flatten()(user_embedding)
+    movie_vec = Flatten()(movie_embedding)
+
+    user_embedding_bias = Flatten()(user_embedding_bias)
+    movie_embedding_bias = Flatten()(movie_embedding_bias)
+
+    user_vec = Dropout(0.5)(user_vec)
+    movie_vec = Dropout(0.5)(movie_vec)
+
+    merged = Concatenate(name='concatenate_of_latent_features')([user_vec, movie_vec])
+
+    biased = Add()([merged, user_embedding_bias, movie_embedding_bias])
+
+    nn = Dense(96, kernel_initializer='he_normal', activation='relu')(biased)
+    nn = Dropout(0.5)(nn)
+
+    nn = BatchNormalization()(nn)
+
+    nn = Dense(1, kernel_initializer='glorot_uniform', activation='sigmoid')(nn)
+
+    nn = Lambda(lambda x: x * (5.0 - 1.0) + 1.0)(nn)
+
+    model = Model(inputs=[user_input, movie_input], outputs=nn)
+    #model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(lr=1e-3), loss='mse')
+
+    return model
+
+
+def run(model, train_data, valid_data):
+    batch_size = 128
+    epochs = 20
+
+    model.fit([train_data.userId, train_data.movieId], train_data.rating,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_data=([valid_data.userId, valid_data.movieId], valid_data.rating),
+              verbose=1
+              )
+
+    return
+
+
+def main():
+    df = import_dataframe()
+
+    users = df.userId.unique()
+    movies = df.movieId.unique()
+
+    userid2idx = {o: i for i, o in enumerate(users)}
+    movieid2idx = {o: i for i, o in enumerate(movies)}
+
+    df['userId'] = df['userId'].apply(lambda x: userid2idx[x])
+    df['movieId'] = df['movieId'].apply(lambda x: movieid2idx[x])
+    split = np.random.rand(len(df)) < 0.8
+    train_data = df[split]
+    valid_data = df[~split]
+
+    n_users = len(df['userId'].unique())
+    n_movies = len(df['movieId'].unique())
+
+    model = keras_nn_model_2(input_dim_1=n_users, input_dim_2=n_movies, n_latent_factors=50)
+
+    # model.summary()
+    run(model, train_data, valid_data)
+
+
+main()
+
+## Embeddings
+# Extract latent features (bias + factor/weights ) (Matrix split intro two matrices: A=G*H)
+# Scale to the most important latent features (PCA)
+
+# X_train = int(bool(data))
+# X_test = int(not bool(data)) + X_train
+
+# print(X_train)
+
+# data[data == 0] = np.nan
+
+####
+# data_pred = IterativeImputer(verbose=2).fit_transform(data)
 
 # data_pred = BiScaler().fit_transform(data)
 # data_pred = SoftImpute().fit_transform(data_pred)
 
 # data_pred = KNN(k=3).fit_transform(data)
 
-# data_pred = NuclearNormMinimization().fit_transform(data)
+# data_pred = NuclearNormMinimization(min_value=1.0, max_value=5.0).fit_transform(data)
+# data_pred = MatrixFactorization().fit_transform(data)
+####
 
-export_data(data_pred, 'franz')
-
-
+# export_data(data_pred, 'franz')
--- franz/porter.py
+++ franz/porter.py
@@ -1,12 +1,14 @@
+import numpy as np
+import pandas as pd
 from scipy import sparse as sp
 
 
-def import_data():
+def import_raw_data(filepath):
     rows = []
     cols = []
     data = []
 
-    with open('../input/data_train.csv', 'r') as file:
+    with open(filepath, 'r') as file:
         header = file.readline()
 
         for line in file:
@@ -19,35 +21,30 @@ def import_data():
             cols.append(col)
             data.append(star_rating)
 
-    csr_matrix = sp.csr_matrix((data, (rows, cols)), dtype=float)
+    return np.c_[rows, cols, data]
 
-    return csr_matrix
+
+def import_dataframe():
+    filepath = '../input/data_train.csv'
+    column_names = ['userId', 'movieId', 'rating']
+    return pd.DataFrame(import_raw_data(filepath), columns=column_names, dtype=np.float64)
+
+
+def import_csr():
+    filepath = '../input/data_train.csv'
+    data = import_raw_data(filepath)
+    return sp.csr_matrix((data[2], (data[0], data[1])), dtype=np.float64)
 
 
 def export_data(extended_matrix, name):
-    rows = []
-    cols = []
-    data = []
+    filepath = '../output/sampleSubmission.csv'
+    data = import_raw_data(filepath)
 
-    with open('../output/sampleSubmission.csv', 'r') as file:
-        file.readline()
-
-        for line in file:
-            entry, prediction = line.split(',')
-            star_rating = int(prediction)
-            row_entry, column_entry = entry.split('_')
-            row = int(row_entry[1:]) - 1
-            col = int(column_entry[1:]) - 1
-            rows.append(row)
-            cols.append(col)
-            data.append(star_rating)
-
-    with open('../output/'+name+'Submission.csv', 'w') as file:
+    with open('../output/' + name + 'Submission.csv', 'w') as file:
         file.write('Id,Prediction\n')
-        for i in range(0, len(rows)):
-            row = rows[i] + 1
-            col = cols[i] + 1
-            file.write('r'+str(row)+'_c'+str(col)+','+str(round(extended_matrix[row-1, col-1])))
+        for i in range(0, len(data[0])):
+            row = data[0][i] + 1
+            col = data[1][i] + 1
+            file.write('r' + str(row) + '_c' + str(col) + ',' + str(round(extended_matrix[row - 1, col - 1])))
             file.write("\n")
-
     return
