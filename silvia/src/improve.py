81c65c2098c4d2cb8fab9eb1ac929f6d5dbeb44a
Silvia La <sla@student.ethz.ch>
2020-07-16 10:15:04 +0200

    fml

--- silvia/src/improve.py
+++ silvia/src/improve.py
@@ -1,5 +1,6 @@
 import numpy as np
-from matplotlib import pyplot as plt
+import random
+import math
 '''
 STEP 2: Improve unknown ratings. Do not change known ratings!
 Using cross validation, we compare the score of different approaches to each
@@ -7,7 +8,7 @@ other and to the score before improving the prediction.
 '''
 
 def run(A: 'numpy.ndarray', ratings: set, asked_ratings: set, users: int,
-        movies: int, approach_id) -> 'numpy.ndarray':
+        movies: int, approach_id, svd_k: int = 0) -> 'numpy.ndarray':
     '''
     This function is called by run.py to improve rating predictions.
 
@@ -18,13 +19,14 @@ def run(A: 'numpy.ndarray', ratings: set, asked_ratings: set, users: int,
     :param users: number of users
     :param movies: number of movies
     :param approach_id: ID of the approach which should be used
+    :param svd_k: number of eigenvalues to keep for SVD
     :return: full rating matrix
     '''
-    return do_improve[approach_id](A, ratings, asked_ratings)
+    return do_improve[approach_id](A, ratings, asked_ratings, svd_k)
 
 
 def svd(A: 'numpy.ndarray', ratings: set,
-        asked_ratings: set) -> 'numpy.ndarray':
+        asked_ratings: set, k: int) -> 'numpy.ndarray':
     '''
     Does SVD on matrix A.
 
@@ -32,23 +34,25 @@ def svd(A: 'numpy.ndarray', ratings: set,
     :param ratings: set of known rating tuples (row, column, rating)
     :param asked_ratings: subset of ratings for which the rating is asked
         for in cross validation
+    :param k: number of eigenvalues to keep
     :return: full rating matrix
     '''
     print('Prediction improvement approach: svd')
     U, d, Vt = np.linalg.svd(A, full_matrices=True)
     D = np.zeros(A.shape)
     D[:A.shape[1], :A.shape[1]] = np.diag(d)
-    k = 13
     Dk = D.copy()
     Dk[k:, k:] = 0
     Ak = U.dot(Dk).dot(Vt)
     return Ak
 
+weight = 0.2
 
-def error_adjustment(A, ratings, asked_ratings):
+def error_adjustment(A: 'numpy.ndarray', ratings: set, asked_ratings: set,
+        k: int) -> 'numpy.ndarray':
     '''
-    Factorize input matrix A = XY. Repeat: For each entry in A where
-    cross validation asks for a prediction, calculate the
+    Factorize input matrix A = XY. Repeat: For each known rating in A where
+    cross validation does not ask for a prediction, calculate the
     error = actual - prediction and adjust the values in XY accrodingly.
     This approach is similar to gradient descend. However, the approach
     minimizes the error linearly and uses the error instead of a gradient.
@@ -57,24 +61,71 @@ def error_adjustment(A, ratings, asked_ratings):
     :param ratings: set of known rating tuples (row, column, rating)
     :param asked_ratings: subset of ratings for which the rating is asked
         for in cross validation
+    :param k: not used
     :return: full rating matrix
     '''
     print('Prediction improvement approach: error adjustment')
-    A_known = np.zeros(A.shape)
-    for (r, c, rating) in ratings:
-        if not (r, c, rating) in asked_ratings:
-            A_known[r,c] = rating
     # Factorize A = (U*D)*Vt = X*Y
-    U, d, Vt = np.linalg.svd(A_known, full_matrices=True)
+    U, d, Vt = np.linalg.svd(A, full_matrices=True)
     D = np.zeros(A.shape)
     D[:A.shape[1], :A.shape[1]] = np.diag(d)
     X = U.dot(D)
     Y = Vt
+    # Create list of tuples (row, column, rating) of known ratings
+    known = list(ratings.copy() - set(asked_ratings))
+    # Adjust
+    random.shuffle(known) 
+    A = error_adjustment_one_cycle(X,Y,known)
+
+
+def error_adjustment_one_cycle(X,Y,known):
+    count = len(known)
+    for (r, c, rating) in known:
+        X[r,:], Y[:,c] = error_adjustment_one_iteration(X[r,:], Y[:,c],
+                rating)
+        count -= 1
+        if count % 1000 == 0:
+            print(count)
+        assert not np.any(np.isnan(X))
+        assert not np.any(np.isnan(Y))
+    return X.dot(Y)
+
+
+def error_adjustment_one_iteration(x, y, actual_rating):
+    error = actual_rating - (x.dot(y))
+    if np.isnan(error):
+        '''
+        print(type(x))
+        print(type(y))
+        print(np.shape(x))
+        print(np.shape(y))
+        print(actual_rating)
+        print(np.any(np.isinf(x)))
+        print(np.any(np.isnan(x)))
+        print(np.any(np.isinf(y)))
+        print(np.any(np.isnan(y)))
+        print(x.dot(y))
+        print(np.dot(x,y))
+        print(np.dot(np.ravel(x), np.ravel(y)))
+        print(np.dot(x,y.T))
+        '''
+        return x, y
+    assert(not np.isnan(error)) 
+    if bool(random.getrandbits(1)):
+        if not np.any(np.isinf(x + error * weight)):
+            x = x + error * weight
+        error = actual_rating - (x.dot(y))
+        if not np.any(np.isinf(y + error * weight)):
+            y = y + error * weight
+    else:
+        if not np.any(np.isinf(y + error * weight)):
+            y = y + error * weight
+        error = actual_rating - (x.dot(y))
+        if not np.any(np.isinf(x + error * weight)):
+            x = x + error * weight
+        #assert not np.any(np.isinf(x)) and not np.any(np.isinf(y))
+    return x, y
     
-
-
-
-
 
 do_improve = {  1: svd,
                 2: error_adjustment}
--- silvia/src/run.py
+++ silvia/src/run.py
@@ -9,7 +9,12 @@ users = 10000
 movies = 1000
 data_path = '../input_data/data_train.csv'
 
-runs = 5
+svd = 1
+error_adjust = 2
+
+svd_k = 17
+
+runs = 1
 A_best = None
 score_best = 100
 
@@ -17,8 +22,6 @@ for i in range(runs):
     print('Run ' + str(i+1) + ' of ' + str(runs) + '..........')
     # Init matrix
     A, ratings = util.csv_to_rating_matrix(data_path, np.nan, users, movies)
-    asked_entries = set()
-
     A, asked_entries = util.split_data(A, ratings, np.full((movies, 1),
         np.nan), True)
 
@@ -28,8 +31,9 @@ for i in range(runs):
     score = util.score(A, asked_entries)
     print(score)
 
-    # Improve
-    A = improve.run(A, ratings, asked_entries, users, movies, 1)
+    # Improve using SVD
+    approach = error_adjust
+    A = improve.run(A, ratings, asked_entries, users, movies, approach, svd_k)
     score = util.score(A, asked_entries)
     print(score)
     if score < score_best:
@@ -37,6 +41,6 @@ for i in range(runs):
         score_best = score
 
 # Write to output file
-output_path = '../output_data/svd_k13_' + str(score_best) + '.csv'
+output_path = '../output_data/error_adjust'+'_'+str(score_best)+'.csv'
 util.create_submission_csv(A_best, output_path)
 
