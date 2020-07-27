a5adc678d81f3366808b5c10965cc2a7222a86ef
Silvia La <sla@student.ethz.ch>
2020-07-27 17:02:42 +0200

    rename
    Former-commit-id: 54a25fa825a63310b47864c2af61ed3bcdb4aa8e

---
+++ final_submission/src/embedding_dot_product_main.py
@@ -0,0 +1,187 @@
+import numpy as np
+import pandas as pd
+import os
+import torch
+import torch.nn as nn
+import fastai
+from fastai.collab import CollabDataBunch, CollabList, collab_learner, data_collate, CollabLearner
+from fastai.collab import AdamW, MSELossFlat, load_learner, DatasetType, Learner, OptRange, Collection, Tuple
+
+from embedding_dot_product_model import CloudModel
+
+
+class Parameters:
+    def __init__(self):
+        # User defined parameters
+        self.EPOCHS = 10
+        self.EMB_SIZE = 25
+        self.BATCH_SIZE = 5000
+        self.DROPOUT = 0.9
+        self.POOL_KERNEL_SIZE = (2, 2)
+        self.CONV_KERNEL_SIZE = (2, 2)
+        self.CNN_DIMS = (128, 256)
+        self.DNN_DIMS = (128,)  # (64, 32, 64)
+        self.MAX_LR = 1e-2
+        self.WEIGHT_DECAY = 1e-1
+        self.SPLIT_VAL_RATE = 0.2
+        self.PREDICT = False
+
+        self.DATA_DIM = [10000, 1000]
+        self.REG = True
+        self.WORKING_PATH = "."
+        self.DATA_SET_PATH = "../input/data_train.csv"
+        self.RES_SET_PATH = "../output/sampleSubmission.csv"
+        self.MODEL_SAVE_PATH = "./models/"
+        self.MODEL_SAVE_NAME = "trained_model.pkl"
+        self.DTYPE = torch.double
+        self.SEED = 42
+
+        # Implicit definition of parameters
+        self.DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.WORKERS = 1 if torch.cuda.is_available() else len(os.sched_getaffinity(0))
+        torch.manual_seed(self.SEED)
+
+        # Assertions
+        assert os.path.isfile(self.DATA_SET_PATH), "DATA_SET_PATH points to no file"
+        assert os.path.isfile(self.RES_SET_PATH), "RES_SET_PATH points to no file"
+
+
+def prophetic_collab_learner(
+        data,
+        parameters,
+        y_range: Tuple[float, float] = None,
+        dropout: float = 0.5,
+        output_dim: int = 1,
+        embed_dim: int = 128,
+        dnn_dims: Collection[int] = (2048, 4096, 512, 1024, 128, 256, 32, 64, 8, 16),
+        input_depth: int = 1,
+        cnn_dims: Collection[int] = (4, 8, 16, 32),
+        conv_kernel_size: Collection[int] = (3, 3),
+        pool_kernel_size: Collection[int] = (2, 2),
+        **learn_kwargs
+) -> Learner:
+    u, m = data.train_ds.x.classes.values()
+
+    model = CloudModel(
+        field_dims=(len(u), len(m)),
+        output_dim=output_dim,
+        embed_dim=embed_dim,
+        y_range=y_range,
+        dropout=dropout,
+        dnn_dims=dnn_dims,
+        input_depth=input_depth,
+        cnn_dims=cnn_dims,
+        conv_kernel_size=conv_kernel_size,
+        pool_kernel_size=pool_kernel_size
+    )
+    model.to(device=parameters.DEVICE)
+    return CollabLearner(data, model, **learn_kwargs)
+
+
+def export_data(data_exp_data):
+    i = 0
+    filename = '../output/franzSubmission{}.csv'.format(i)
+
+    while os.path.isfile(filename):
+        filename = '../output/franzSubmission{}.csv'.format(i)
+        i += 1
+
+    with open(filename, 'w') as file:
+        file.write('Id,Prediction')
+        for idx in range(0, len(data_exp_data)):
+            row = int(data_exp_data[idx][0])
+            col = int(data_exp_data[idx][1])
+            val = data_exp_data[idx][2]
+            if val < 1.0:
+                val = 1.0
+            if val > 5.0:
+                val = 5.0
+            file.write("\n")
+            file.write('r' + str(row) + '_c' + str(col) + ',' + str(val))
+    return
+
+
+def import_data(path, drop_predictions=False):
+    import_data_df = pd.read_csv(path, low_memory=False)
+    import_data_df[['userId', 'movieId']] = import_data_df['Id'].str.split("_", n=1, expand=True)
+    import_data_df[['userId']] = import_data_df['userId'].str.strip("r")
+    import_data_df[['movieId']] = import_data_df['movieId'].str.strip("c")
+    if drop_predictions:
+        import_data_df = import_data_df[['userId', 'movieId']].astype(np.long)
+    else:
+        import_data_df = import_data_df[['userId', 'movieId', 'Prediction']].astype(np.long)
+
+    return import_data_df
+
+
+def main():
+    parameters = Parameters()
+    fastai.device = parameters.DEVICE
+
+    print("Device: {}".format(parameters.DEVICE))
+    print("== Loading Data ==")
+    df = import_data(parameters.DATA_SET_PATH, drop_predictions=False)
+    tf = import_data(parameters.RES_SET_PATH, drop_predictions=True)
+
+    data = CollabDataBunch.from_df(
+        ratings=df,
+        valid_pct=parameters.SPLIT_VAL_RATE,
+        test=None,
+        seed=parameters.SEED,
+        path='.',
+        bs=parameters.BATCH_SIZE,
+        val_bs=None,
+        num_workers=parameters.WORKERS,
+        dl_tfms=None,
+        device=parameters.DEVICE,
+        collate_fn=data_collate,
+        no_check=False
+    )
+
+    y_range = (0.5, 5.5)
+
+    learn = prophetic_collab_learner(
+        data,
+        parameters,
+        y_range=y_range,
+        dropout=parameters.DROPOUT,
+        output_dim=1,
+        embed_dim=parameters.EMB_SIZE,
+        dnn_dims=parameters.DNN_DIMS,
+        input_depth=1,
+        cnn_dims=parameters.CNN_DIMS,
+        conv_kernel_size=parameters.CONV_KERNEL_SIZE,
+        pool_kernel_size=parameters.POOL_KERNEL_SIZE,
+
+        wd=parameters.WEIGHT_DECAY,
+        opt_func=AdamW,
+        loss_func=nn.MSELoss(),  # CrossEntropyFlat, MSELossFlat, BCEFlat, BCEWithLogitsFlat
+        metrics=None
+    )
+
+    print("== Start Training ==")
+    learn.unfreeze()
+    # learn.lr_find()
+    # learn.recorder.plot()
+
+    learn.fit_one_cycle(cyc_len=parameters.EPOCHS, max_lr=parameters.MAX_LR, wd=parameters.WEIGHT_DECAY)
+    learn.fit_one_cycle(cyc_len=parameters.EPOCHS, max_lr=parameters.MAX_LR, wd=parameters.WEIGHT_DECAY)
+    learn.fit_one_cycle(cyc_len=parameters.EPOCHS, max_lr=parameters.MAX_LR, wd=parameters.WEIGHT_DECAY)
+    learn.fit_one_cycle(cyc_len=parameters.EPOCHS, max_lr=parameters.MAX_LR, wd=parameters.WEIGHT_DECAY)
+    learn.export(parameters.MODEL_SAVE_PATH + parameters.MODEL_SAVE_NAME)
+    print("== Finished Training ==")
+
+    if parameters.PREDICT:
+        print("== Start Predicting ==")
+        learn = load_learner(path=parameters.MODEL_SAVE_PATH, file=parameters.MODEL_SAVE_NAME, test=CollabList.from_df(tf))
+        y_pred, _ = learn.get_preds(ds_type=DatasetType.Test)
+        print("== Finished Predicting ==")
+
+        print("== Saving Predictions ==")
+        tf['Predictions'] = y_pred.numpy()
+        export_data(tf.to_numpy())
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ final_submission/src/embedding_dot_product_model.py
@@ -0,0 +1,176 @@
+import math
+from typing import Tuple, Collection
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from fastai.layers import embedding, LongTensor
+
+
+class CloudModel(nn.Module):
+    def __init__(
+            self,
+            field_dims,
+            output_dim,
+            embed_dim,
+            y_range,
+            dropout,
+            dnn_dims,
+            input_depth,
+            cnn_dims,
+            conv_kernel_size,
+            pool_kernel_size
+    ):
+        super(CloudModel, self).__init__()
+        self.y_range = y_range
+
+        size = embed_dim
+        for i in range(0, len(cnn_dims)):
+            size = math.floor((size - conv_kernel_size[0] + 1) / pool_kernel_size[0])
+        self.cnn_output_size = size * size * cnn_dims[-1]
+
+        n_users = field_dims[0]
+        n_items = field_dims[1]
+        n_factors = embed_dim
+        (self.u_weight, self.i_weight, self.u_bias, self.i_bias) = [embedding(*o) for o in [
+            (n_users, n_factors), (n_items, n_factors), (n_users, 1), (n_items, 1)
+        ]]
+
+        self.embed_output_dim = len(field_dims) * embed_dim
+        self.cnn = CNN(input_depth, cnn_dims, dropout, conv_kernel_size, pool_kernel_size)
+
+        dnn_input_length = n_factors*9 + self.cnn_output_size + 5
+        dnn_input_length = self.cnn_output_size
+
+        self.dnn = DNN(dnn_input_length, output_dim, dnn_dims, dropout)
+        self.stdlin = StandardLinear(input_size=1, output_size=1, dropout=0.5)
+        self.enhance = None
+
+    def forward(self, users: LongTensor, items: LongTensor):
+        # Get embeddings
+        u, v = self.u_weight(users), self.i_weight(items)
+
+        # Extract correlations
+        ## Euclidean Distance
+        '''
+        euclid_squared, euclid = minkovski(u, v, 2)
+        minkovski_squared_3, minkovski_3 = minkovski(u, v, 3)
+        minkovski_squared_4, minkovski_4 = minkovski(u, v, 4)
+        minkovski_squared_5, minkovski_5 = minkovski(u, v, 5)
+
+        u_squared = u.square()
+        v_squared = v.square()
+
+        sin_u = u.sin()
+        sin_v = v.sin()
+        cos_u = u.cos()
+        cos_v = v.cos()
+
+        pythagoras = (u_squared + v_squared).sqrt()
+        
+        # Inner product
+        inner = u * v
+        inner = inner.sum(1) + self.u_bias(users).squeeze() + self.i_bias(items).squeeze()
+        inner = inner.unsqueeze(-1)
+        #inner = self.stdlin(inner)
+        '''
+        # Outer product
+        outer = torch.einsum('bp, bq->bpq', u, v).unsqueeze(1)
+        # CNN
+        cnn = self.cnn(outer)
+
+        # Concatenate outputs
+        '''
+        concat = torch.cat(
+            (
+                u,
+                v,
+                inner,
+                euclid_squared,
+                euclid,
+                u_squared,
+                v_squared,
+                sin_u,
+                sin_v,
+                cos_u,
+                cos_v,
+                pythagoras,
+                self.u_bias(users),
+                self.i_bias(items),
+                cnn
+            ),
+            1)
+        '''
+        concat = cnn
+        # DNN
+        dnn = self.dnn(concat)
+
+        # Last step
+        x = dnn #+ inner
+        if self.y_range is not None:
+            x = torch.sigmoid(x) * (self.y_range[1] - self.y_range[0]) + self.y_range[0]
+
+
+        return torch.flatten(x)
+
+
+class DNN(nn.Module):
+    def __init__(self, input_dim, output_dim, dnn_dims, dropout, output_layer=True):
+        super(DNN, self).__init__()
+        layers = list()
+        for dnn_dim in dnn_dims:
+            layers.append(nn.Linear(in_features=input_dim, out_features=dnn_dim, bias=True))
+            layers.append(nn.Dropout(p=dropout))
+            layers.append(nn.PReLU())
+            layers.append(nn.BatchNorm1d(num_features=dnn_dim))
+            input_dim = dnn_dim
+        if output_layer:
+            layers.append(nn.Linear(in_features=input_dim, out_features=output_dim))
+        self.dnn = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.dnn(x)
+
+
+class CNN(nn.Module):
+    def __init__(self, input_depth, cnn_dims, dropout, conv_kernel_size, pool_kernel_size):
+        super(CNN, self).__init__()
+        layers = list()
+        for output_depth in cnn_dims:
+            layers.append(nn.Conv2d(
+                in_channels=input_depth,
+                out_channels=output_depth,
+                kernel_size=conv_kernel_size,
+                bias=True))
+            layers.append(nn.MaxPool2d(
+                kernel_size=pool_kernel_size,
+            ))
+            layers.append(nn.Dropout(p=dropout))
+            layers.append(nn.PReLU())
+            layers.append(nn.BatchNorm2d(num_features=output_depth))
+            input_depth = output_depth
+        layers.append(nn.Flatten())
+        self.cnn = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.cnn(x)
+
+
+class StandardLinear(nn.Module):
+    def __init__(self, input_size, output_size, dropout):
+        super(StandardLinear, self).__init__()
+        self.perceptron = nn.Linear(in_features=input_size, out_features=output_size)
+        self.dropout = nn.Dropout(p=dropout)
+        self.prelu = nn.PReLU()
+        self.bn = nn.BatchNorm1d(num_features=output_size)
+
+    def forward(self, x):
+        x = self.perceptron(x)
+        x = self.prelu(x)
+        x = self.bn(x)
+        return x
+
+
+def minkovski(u: torch.Tensor, v: torch.Tensor, p: int = 1):
+    tmp = (u - v).pow(p).sum(1)
+    return tmp.unsqueeze(-1), tmp.pow(1/p).unsqueeze(-1)
